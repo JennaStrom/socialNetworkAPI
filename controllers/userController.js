061689e67a3efcb4a235463a42c25617fccb440e
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-07-11 20:55:02 -0500

    finished up all the code, just need to do some more insomnia testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,101 @@
-const { User, Thought } = require('../models')
+const { User } = require('../models')
 
 // get all users 
 // get one user by ID
 module.exports = {
     async getUsers(req, res) {
-      try {
-        const users = await User.find();
-        res.json(users);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
     async getSingleUser(req, res) {
-      try {
-        const user = await User.findOne({ _id: req.params.userId })
-          .select('-__v');
-  
-        if (!user) {
-          return res.status(404).json({ message: 'No user with that ID' });
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
         }
-  
-        res.json(user);
-      } catch (err) {
-        res.status(500).json(err);
-      }
     },
-// add a new user
-async createUser(req, res) {
-    try {
-      const dbUserData = await User.create(req.body);
-      res.json(dbUserData);
-    } catch (err) {
-      res.status(500).json(err);
+    // add a new user
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // update user info
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate({ _id: req.params.userId }, {
+                $set: req.body
+            }, {
+                runValidators: true,
+                new: true
+            })
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // delete user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // add a friend to user
+    async addFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // remove a friend from user 
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: { friendId: req.params.friendId } } },
+                { runValidators: true, new: true }
+            )
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
-  },
 };
-
-// update user info
-
-// delete user
-
-// add a friend to user
-
-// remove a friend from user 
--- models/Thought.js
+++ models/Thought.js
@@ -1,36 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
-
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-)
-
-thoughtSchema.virtual('reactionCount')
-    .get(function () {
-        return this.reactions.length;
-    })
+const moment = require('moment')
 
 const reactionSchema = new Schema(
     {
@@ -61,6 +30,40 @@ const reactionSchema = new Schema(
         id: false,
     }
 )
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(function () {
+        return this.reactions.length;
+    })
+
+
 
 const Thought = model('Thought', thoughtSchema);
 
